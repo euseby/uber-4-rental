c3f06ce22c3e9664d82a18652413b2eb7670022d
Fodor Eusebiu <euseby779@gmail.com>
2026-03-12 22:43:33 +0200

    Spring-boot-start-app

---
+++ src/main/java/org/eusebiu/UberRentalApplication.java
@@ -0,0 +1,11 @@
+package org.eusebiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberRentalApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UberRentalApplication.class, args);
+    }
+}
